f7372d2a4e51c185dffe3b33adaa6ebddd889e79
Jason Yebra <jasonyebra@gmail.com>
2022-08-30 13:18:43 -0500

    first commit

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$to = "jasonyebra@gmail.com"; // this is your Email address (make a wedding email)
+	$from  = $_POST['email']; // this is the sender's Email address
+	$sender_name = $_POST['name'];
+	$number_of_gustes = $_POST['guest'];
+	$events = $_POST['events'];
+	$phone = $_POST['phone'];
+	$address = $_POST['address'];
+	$notes = $_POST['notes'];
+
+
+	$subject = "Form submission";
+	$message = $sender_name . " is attending! The number of gustes of his / her is : " .  $number_of_gustes . " and his / her selected event is " . $events . ". He / she worte the following... ". "\n\n" . $notes;
+
+	$headers = 'From: ' . $from;
+	mail($to, $subject, $message, $headers);
+
+?>
